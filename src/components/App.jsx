2ef5aad5cb32ee2ebabb09d2e2a714103a3258aa
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-05-07 00:52:14 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { NavLink, Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="movies">Movies</NavLink></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<div>Home</div>}/>
+        <Route path="/movies" element={<div>Movies</div>}/>
+        <Route path="/movies/:movieId" element={<div>MovieDetails</div>}>
+          <Route path="cast" element={<div>Cast</div>}/>
+          <Route path="reviews" element={<div>Reviews</div>}/>
+        </Route>
+      </Routes>
     </div>
   );
 };
